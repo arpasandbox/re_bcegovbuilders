329c30405b0ca9cc0008560581a7e1fed8a000fd
bttpkristina <arpasandbox@gmail.com>
2026-01-14 21:42:41 +0800

    update

--- src/components/PastPerformance.tsx
+++ src/components/PastPerformance.tsx
@@ -60,7 +60,7 @@ const PastPerformance = () => {
       description: 'COURSE OF CONSTRUCTION',
       location:'BELLEVUE, WA',
       price:'',
-      image: '/images/projects/project_21.png',
+      image: '/images/projects/project_4.png',
     },
     {
       name: 'SAULSBURY',
